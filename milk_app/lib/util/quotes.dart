f6e56b0451c0ee50f0a6b0189e8a7d7fceeb1de4
JP-2000 <jitenpatel1482000@gmail.com>
2020-07-12 15:30:37 +0530

    databsse directory changed

--- milk_app/lib/util/quotes.dart
+++ milk_app/lib/util/quotes.dart
@@ -7,10 +7,7 @@ class Quotes{
   static List images_list = [
     "assets/images/dummy1.jpg",
     "assets/images/dummy2.jpg",
-<<<<<<< HEAD
-    "assets/images/dummy3.jpg"
-=======
     "assets/images/dummy3.jpg",
->>>>>>> b9899cf529f10ad8f81108e168e6161d717eb63a
+    "assets/images/dummy3.jpg",
   ];
 }
